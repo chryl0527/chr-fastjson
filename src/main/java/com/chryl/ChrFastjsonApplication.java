c63d53a40c38462f1bad249adfa7d6fd1282fe5c
chr0527 <1015670600@qq.com>
2019-05-24 14:45:15 +0800

    Initial commit

---
+++ src/main/java/com/chryl/ChrFastjsonApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrFastjsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrFastjsonApplication.class, args);
+	}
+
+}

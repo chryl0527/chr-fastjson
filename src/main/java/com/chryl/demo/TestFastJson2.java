40e4536b2719be8db20c38c4418a46c6107a4681
Chr0527 <1015670600@qq.com>
2019-07-15 12:05:45 +0800

    init

---
+++ src/main/java/com/chryl/demo/TestFastJson2.java
@@ -0,0 +1,45 @@
+package com.chryl.demo;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 解析httpclient得到的数据
+ * <p>
+ * Created By Chr on 2019/7/15.
+ */
+public class TestFastJson2 {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void show() {
+        String url = "http://127.0.0.1:8099/sb/queryAll";
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(url, String.class);
+        String body = forEntity.getBody();
+
+        System.out.println(body);
+
+        String jsonString = JSON.toJSONString(body);
+        System.out.println(jsonString);
+
+        //
+        System.out.println("===");
+        JSONObject jsonObject = JSONObject.parseObject(body);
+        String statusStr = jsonObject.getString("status");
+        if (statusStr.equals("success")) {
+            String dataStr = jsonObject.getString("data");
+            List<Map<String, String>> list = JSON.parseObject(dataStr, List.class);
+            System.out.println(list);
+            for (Map<String, String> stringStringMap : list) {
+                System.out.println(stringStringMap);
+            }
+        }
+    }
+}
